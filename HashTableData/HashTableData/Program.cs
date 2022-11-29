83e7e075d7c2af50e81ce7695e084ff65a87b50f
Snehal979 <bansodsnehal535@gmail.com>
2022-11-29 19:05:05 +0530

    (Snehal)Add:Main program for hashtable

---
+++ HashTableData/HashTableData/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HashTableData
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hashtable");
+
+           
+
+
+
+        }
+    }
+}
