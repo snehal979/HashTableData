12051cf558b44db76e52810cd7e14b599348cacb
Snehal979 <bansodsnehal535@gmail.com>
2022-11-29 19:34:00 +0530

    (Snehal)Add: fequency of word

--- HashTableData/HashTableData/Program.cs
+++ HashTableData/HashTableData/Program.cs
@@ -7,14 +7,37 @@
 
             Console.WriteLine("Hashtable");
             HashingStructure<int, string> list = new HashingStructure<int, string>(10);
-            list.Add(1, "to");
-            list.Add(2, "be");
-            list.Add(3, "or");
-            list.Add(4, "not");
-            list.Add(5, "to");
-            list.Add(6, "be");
-            
+            list.Add(1, "Paranoids");
+            list.Add(2, "are");
+            list.Add(3, " not");
+            list.Add(4, "paranoid");
+            list.Add(5, "because");
+            list.Add(6, "they");
+            list.Add(7, "are");
+            list.Add(8, "paranoid");
+            list.Add(9, "but");
+            list.Add(10, "because");
+            list.Add(11, "they");
+            list.Add(12, "keep");
+            list.Add(13, "putting");
+            list.Add(14, "themselves");
+            list.Add(15, "deliberately");
+            list.Add(16, "into ");
+            list.Add(17, "paranoid");
+            list.Add(18, "avoidable");
+            list.Add(19, "situations");
 
+            for(int i =1; i < 20; i++)
+            {
+                string hash5 = list.Get(i);
+                Console.WriteLine("{0}th index value {1} ",i,hash5);
+
+            }
+
+
+
+
+            Console.WriteLine(list.GetHashCode()); 
 
 
 
